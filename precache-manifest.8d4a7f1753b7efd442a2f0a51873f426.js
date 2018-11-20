338e3782b4ac39dcd93d923ffec9af2580bfa118
Adam <adam.a@spartadigital.co.uk>
2018-11-20 21:32:54 +0000

    Updates

---
+++ precache-manifest.8d4a7f1753b7efd442a2f0a51873f426.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b841ca48ae7b9b34f01d",
+    "url": "/footy-scores-fixtures/static/js/runtime~main.b841ca48.js"
+  },
+  {
+    "revision": "296faba11198ba7a6fab",
+    "url": "/footy-scores-fixtures/static/js/main.296faba1.chunk.js"
+  },
+  {
+    "revision": "e18680d5f84251fad2b2",
+    "url": "/footy-scores-fixtures/static/js/1.e18680d5.chunk.js"
+  },
+  {
+    "revision": "296faba11198ba7a6fab",
+    "url": "/footy-scores-fixtures/static/css/main.13754ae7.chunk.css"
+  },
+  {
+    "revision": "d51469007f766cdbd8eb55136ff7bff6",
+    "url": "/footy-scores-fixtures/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/footy-scores-fixtures/precache-manifest.8d4a7f1753b7efd442a2f0a51873f426.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/footy-scores-fixtures/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
